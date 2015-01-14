4458d9625d3c75f1a034af09a3e4adeaabef8ced
Bridget Almas <balmas@gmail.com>
2015-01-14 21:35:13 +0000

    ontology update
    first pass at configuring the deployment ontology for image collections in Perseids.
    There are some remaining terms that still need to be corrected --- these are mainly the
    app specific fields, for which, if we use a custom ontology, I'd like the namespace
    to be something different, but I'm not sure what. For now, the official fields of the cite
    collection items should be mostly what we want. Need now to verify this by uploading some
    items from an existing collection.

--- angular/config.js
+++ angular/config.js
@@ -11,8 +11,8 @@ app.service( 'config', [ 'host', function( host ){
         
         // URL to Perseids user data object
         
-        ping: 'conf/ping.js'
-        //ping: 'http://sosol.perseids.org/sosol/dmm_api/ping'
+        // ping: 'conf/ping.js'
+        ping: 'http://sosol.perseids.org/sosol/dmm_api/ping'
       },
       
       urn_serv: {
@@ -30,7 +30,6 @@ app.service( 'config', [ 'host', function( host ){
         
         // URL to SPARQL query endpoint
         
-                                //url: host.url+'/query' 
         url: 'http://services.perseids.org/fuseki/ds/query'
       },
       
@@ -38,17 +37,12 @@ app.service( 'config', [ 'host', function( host ){
         
         // URL to JackSON server
         // JackSON server hosts JSON data and the application by default
-      
-                                //ur: host.url  ,
+        
         url: 'http://www.perseids.org/jackson',
         
-        // Add user value to data
-        
-        tack_on: [ 'user' ]
       }
     },
     imgup: {
-                        //url: 'http://localhost:1234/upload'     
       url: 'http://www.perseids.org/imgup/upload'  
     },
 
@@ -65,13 +59,13 @@ app.service( 'config', [ 'host', function( host ){
                   }, 
                   type: {
                      term: "type",
-                     ns: "https://github.com/PerseusDL/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md#",
-                     prefix: "this"
+                     ns: "http://purl.org/dc/terms/",
+                     prefix: "dct"
                   },
                   src: {
-                     term: "src",
-                     ns: "https://github.com/PerseusDL/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md#",
-                     prefix: "this"
+                     term: "references",
+                     ns: "http://purl.org/dc/terms/",
+                     prefix: "dct"
                   },
                   orig:  {
                      term: "orig",
@@ -79,14 +73,24 @@ app.service( 'config', [ 'host', function( host ){
                      prefix: "this"
                   },
                   creator: {
-                     term: "users/",
-                     ns: "http://data.perseus.org/sosol/",
-                     prefix: "user"
+                     term: "creator",
+                     ns: "http://purl.org/dc/terms/",
+                     prefix: "dct"
+                  },
+                  contributor: {
+                     term: "contributor",
+                     ns: "http://purl.org/dc/terms/",
+                     prefix: "dct"
                   },
                   created: {
-                     term: "dateTime",
-                     ns: "http://www.w3.org/TR/xmlschema11-2/#",
-                     prefix: "xsd"
+                     term: "created",
+                     ns: "http://purl.org/dc/terms/",
+                     prefix: "dct"
+                  },
+                  modified: {
+                     term: "modified",
+                     ns: "http://purl.org/dc/terms/",
+                     prefix: "dct"
                   },
                   width: {
                      term: "width",
@@ -99,9 +103,9 @@ app.service( 'config', [ 'host', function( host ){
                      prefix: "this"
                   },
                   subject: {
-                     term: "keyword",
-                     ns: "https://github.com/PerseusDL/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md#",
-                     prefix: "this"
+                     term: "subject",
+                     ns: "http://purl.org/dc/terms/",
+                     prefix: "dct"
                   },
                   memberOf: {
                      term: "belongsTo",
@@ -113,15 +117,11 @@ app.service( 'config', [ 'host', function( host ){
                      ns: "http://www.cidoc-crm.org/cidoc-crm/",
                      prefix: "crm"
                   },
-                  upload_ref: {
-                     term: "upload",
-                     ns: "https://github.com/PerseusDL/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md#",
-                     prefix: "this"
-                  },
-                  upload_src: {
-                     term: "src",
-                     ns: "https://github.com/PerseusDL/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md#",
-                     prefix: "this"
+                  rights: {
+                     term: "license",
+                     ns: "http://www.homermultitext.org/cite/rdf/",
+                     prefix: "cite",
+                     default_value: "http://creativecommons.org/licenses/by-nc-sa/4.0/"
                   }
                 }
   })
