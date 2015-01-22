a9b2a77365aae33542f1191b2668f96386496eef
caesarfeta <adamtavares@gmail.com>
2015-01-22 17:01:06 -0500

    Added img-loc directive for displaying img ROI context.

--- angular/config.js
+++ angular/config.js
@@ -11,7 +11,7 @@ app.service( 'config', [ 'host', function( host ){
         
         // URL to Perseids user data object
         
-        // ping: 'conf/ping.js'
+        //ping: 'conf/ping.js'
         ping: 'http://sosol.perseids.org/sosol/dmm_api/ping'
       },
       
@@ -45,99 +45,98 @@ app.service( 'config', [ 'host', function( host ){
     imgup: {
       url: 'http://www.perseids.org/imgup/upload'  
     },
-
-                ontology: {
-                  imgViewer: {
-                    term: "imageViewer",
-                    ns: "http://data.perseus.org/rdfvocab/cite/",
-                    prefix: "citex"
-                  },
-                  imgServer: {
-                    term: "imageServer",
-                    ns: "http://data.perseus.org/rdfvocab/cite/",
-                    prefix: "citex"
-                  },
-                  label:  {
-                     term: "label",
-                     ns: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-                     prefix: "rdf"
-                  },
-                  description: {
-                     term: "description",
-                     ns: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-                     prefix: "rdf"
-                  }, 
-                  type: {
-                     term: "type",
-                     ns: "http://purl.org/dc/terms/",
-                     prefix: "dct"
-                  },
-                  src: {
-                     term: "references",
-                     ns: "http://purl.org/dc/terms/",
-                     prefix: "dct"
-                  },
-                  orig:  {
-                     term: "orig",
-                     ns: "https://github.com/PerseusDL/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md#",
-                     prefix: "this"
-                  },
-                  owner: {
-                     term: "publisher",
-                     ns: "http://purl.org/dc/terms/",
-                     prefix: "dct"
-                  },
-                  creator: {
-                     term: "creator",
-                     ns: "http://purl.org/dc/terms/",
-                     prefix: "dct"
-                  },
-                  contributor: {
-                     term: "contributor",
-                     ns: "http://purl.org/dc/terms/",
-                     prefix: "dct"
-                  },
-                  created: {
-                     term: "created",
-                     ns: "http://purl.org/dc/terms/",
-                     prefix: "dct"
-                  },
-                  modified: {
-                     term: "modified",
-                     ns: "http://purl.org/dc/terms/",
-                     prefix: "dct"
-                  },
-                  width: {
-                     term: "width",
-                     ns: "https://github.com/PerseusDL/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md#",
-                     prefix: "this"
-                  },
-                  height: {
-                     term: "height",
-                     ns: "https://github.com/PerseusDL/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md#",
-                     prefix: "this"
-                  },
-                  subject: {
-                     term: "subject",
-                     ns: "http://purl.org/dc/terms/",
-                     prefix: "dct"
-                  },
-                  memberOf: {
-                     term: "belongsTo",
-                     ns: "http://www.homermultitext.org/cite/rdf/",
-                     prefix: "cite"
-                  },
-                  represents: {
-                     term: "P138_represents",
-                     ns: "http://www.cidoc-crm.org/cidoc-crm/",
-                     prefix: "crm"
-                  },
-                  rights: {
-                     term: "license",
-                     ns: "http://www.homermultitext.org/cite/rdf/",
-                     prefix: "cite",
-                     default_value: "http://creativecommons.org/licenses/by-nc-sa/4.0/"
-                  }
-                }
+	ontology: {
+	  imgViewer: {
+	    term: "imageViewer",
+	    ns: "http://data.perseus.org/rdfvocab/cite/",
+	    prefix: "citex"
+	  },
+	  imgServer: {
+	    term: "imageServer",
+	    ns: "http://data.perseus.org/rdfvocab/cite/",
+	    prefix: "citex"
+	  },
+	  label:  {
+	     term: "label",
+	     ns: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+	     prefix: "rdf"
+	  },
+	  description: {
+	     term: "description",
+	     ns: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+	     prefix: "rdf"
+	  }, 
+	  type: {
+	     term: "type",
+	     ns: "http://purl.org/dc/terms/",
+	     prefix: "dct"
+	  },
+	  src: {
+	     term: "references",
+	     ns: "http://purl.org/dc/terms/",
+	     prefix: "dct"
+	  },
+	  orig:  {
+	     term: "orig",
+	     ns: "https://github.com/PerseusDL/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md#",
+	     prefix: "this"
+	  },
+	  owner: {
+	     term: "publisher",
+	     ns: "http://purl.org/dc/terms/",
+	     prefix: "dct"
+	  },
+	  creator: {
+	     term: "creator",
+	     ns: "http://purl.org/dc/terms/",
+	     prefix: "dct"
+	  },
+	  contributor: {
+	     term: "contributor",
+	     ns: "http://purl.org/dc/terms/",
+	     prefix: "dct"
+	  },
+	  created: {
+	     term: "created",
+	     ns: "http://purl.org/dc/terms/",
+	     prefix: "dct"
+	  },
+	  modified: {
+	     term: "modified",
+	     ns: "http://purl.org/dc/terms/",
+	     prefix: "dct"
+	  },
+	  width: {
+	     term: "width",
+	     ns: "https://github.com/PerseusDL/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md#",
+	     prefix: "this"
+	  },
+	  height: {
+	     term: "height",
+	     ns: "https://github.com/PerseusDL/CITE-JSON-LD/blob/master/templates/img/SCHEMA.md#",
+	     prefix: "this"
+	  },
+	  subject: {
+	     term: "subject",
+	     ns: "http://purl.org/dc/terms/",
+	     prefix: "dct"
+	  },
+	  memberOf: {
+	     term: "belongsTo",
+	     ns: "http://www.homermultitext.org/cite/rdf/",
+	     prefix: "cite"
+	  },
+	  represents: {
+	     term: "P138_represents",
+	     ns: "http://www.cidoc-crm.org/cidoc-crm/",
+	     prefix: "crm"
+	  },
+	  rights: {
+	     term: "license",
+	     ns: "http://www.homermultitext.org/cite/rdf/",
+	     prefix: "cite",
+	     default_value: "http://creativecommons.org/licenses/by-nc-sa/4.0/"
+	  }
+	}
   })
 }])
--- angular/directives.js
+++ angular/directives.js
@@ -95,7 +95,6 @@ function(){
 });
 
 // img-bit functionality.
-
 // see .img-bit in app.scss
 
 appDirectives.directive('imgBit', 
@@ -103,28 +102,107 @@ function(){
   return {
     link: function( scope, elem, attr ){
 		
-		elem.bind('load', 
-		function(e){
+		elem.bind('load', function(e){
+		  
+		  // Build the necessary dom elements
+			
           elem.wrap( '<div class="frame">' );
           var frame = elem.parent();
+		  
           frame.wrap( '<div class="img-bit">' );
+		  var wrap = elem.parent();
+		  
           var height = this.naturalHeight;
           var width = this.naturalWidth;
           var param = attr.ngParam.split(',');
-		     
+		  
+		  // CSS makes the magic happen
+		  
+		  wrap.css({
+		    display: "inline-block"
+		  })
+		   
           frame.css({
             width: parseInt( width*param[2] ),
-            height: parseInt( height*param[3] )
+            height: parseInt( height*param[3] ),
+			position: "relative",
+			overflow: "hidden"
           });
 		     
           elem.css({
+			position: "absolute",
+			display: "block",
+			"vertical-align": "baseline",
+			"max-width": "none",
+			"max-height": "none",
             width: width,
             height: height,
             top: parseInt( param[1]*height*-1 ),
-            left: parseInt( param[0]*width*-1 )
+            left: parseInt( param[0]*width*-1 ),
           });
+		  
 	  	});
-		
+
     }
   }
+});
+
+// img-loc
+
+appDirectives.directive('imgLoc',
+function(){
+  return {
+	  link: function( scope, elem, attr ){
+		  
+		elem.bind('load', function(e){
+			
+			// Get the params you need
+			
+			var param = attr.ngParam.split(',');
+			
+            var height = this.naturalHeight;
+            var width = this.naturalWidth;
+			
+			// Workout max width
+			
+			var max_width = param[4];
+			var diff = max_width / width;
+			if ( diff < 1 ){
+				width = max_width
+				height = height*diff
+			}
+			
+            elem.wrap( '<div class="img-loc">' );
+            var wrap = elem.parent();
+			
+  		  	// CSS makes the magic happen
+			// see .img-loc in app.scss to customize
+		   
+            wrap.css({
+              width: parseInt( width ),
+              height: parseInt( height ),
+			  position: "relative",
+			  overflow: "hidden"
+            });
+			
+			elem.css({
+              width: parseInt( width ),
+			  height: parseInt( height )
+			})
+			
+			wrap.append( '<div class="lite">' );
+			var lite = elem.next();
+			lite.css({
+			    position: "absolute",
+				left: parseInt( width * param[0] ),
+				top: parseInt( height * param[1] ),
+				width: parseInt( width * param[2] ),
+				height: parseInt( height * param[3] )
+			});
+			wrap.after( '<div class="clearfix"></div>' );
+		});
+		
+	  }
+  }
+	
 });
