b9b9f1b99f308878da7e7cead809ff4e70791878
caesarfeta <adamtavares@gmail.com>
2015-02-09 10:36:44 -0500

    Resizer service is working seamlessly.

--- angular/lib/xhr/resizer.js
+++ angular/lib/xhr/resizer.js
@@ -1,94 +1,82 @@
+app.service( 'resizer', [
+'json',
+'imgup',
+'config',
+'urnServ',
+'onto',
+function( json, imgup, config, urnServ, onto ) {
+	
+	var max_width = null;
+	var max_height = null;
+	var src = null;
+	var upload = null;
+	var resize_urn = null;
+	var tmpl = null;
+	var urn = null;
+	
+	return {
+		add: add
+	}
+	
+	function add( _urn, _max_width, _max_height ){
+		max_width = _max_width;
+		max_height = _max_height;
+		urn = _urn;
+		return upload_src()
+	}
+	
+	function upload_src(){
+		return json.urn( urn ).then( 
+		function( data ){
+			src = data['src'][0];
+			return upload_json();
+		});
+	}
+	
+	function upload_json(){
+		return json.get( src ).then(
+		function( data ){
+			upload = data;
+			return get_urn();
+		});
+	}
+	
+	function get_urn(){
+		return urnServ.fresh( urnServ.base+"resize.{{ id }}", function( urn ){
+			resize_urn = urn;
+			return resize_tmpl();
+		});
+	}
+	
+	function resize_tmpl(){
+		return json.get( 'default/resize.json' ).then( function( data ){
+			tmpl = data;
+			return set_vals();
+		});
+	}
+	
+	function set_vals(){
+		tmpl['@id'] = resize_urn;
+		tmpl[onto.with_prefix('memberOf')]['@id'] = upload['@id'];
+		return send();
+	}
+	
+	function send(){
+		var save_to = config.xhr.json.url+'/data/resize/'+resize_urn;
+		var img = upload[ onto.with_prefix('src') ]['@id'];
+		return imgup.resize( img, 200, 200, save_to, tmpl ).then( function( data ){
+			console.log( data );
+		});
+	}
+	
+}]);
+
 /*
 
-############### FIRST SKETCH
+To run this in the console...
 
-# Get the services
-
-var imgup = tserv('imgup');
-var json = tserv('json');
-var urn = tserv('urnServ');
-var onto = tserv('onto');
-
-var resize_urn = 'urn:cite:perseus:resize.ry897TREW'
-var upload_urn = 'urn:cite:perseus:upload.7ISACSKTGEY';
-var src = 'http://localhost:1234/upload/2015/FEB/manuscript.jpg';
-var jackson = 'http://localhost:4567/data/resize/urn:cite:perseus:resize.ry897TREW';
-var tmpl = null;
-json.get('default/resize.json').then( 
-function( data ){
-	tmpl = data;
-});
-
-# Fill out the template
-
-tmpl['@id'] = resize_urn;
-tmpl['cite:belongsTo']['@id'] = upload_urn;
-
-# Resize
-
-imgup.resize( src, 200, 200, jackson, tmpl ).then( function( data ){
-	console.log( data );
-});
-
-
-############ SECOND DRAFT
-
-var imgup = tserv('imgup');
-var json = tserv('json');
-var urnServ = tserv('urnServ');
-var onto = tserv('onto');
-var user = tserv('user');
-
-# This is all I'll start with...
-
-var upload_urn = 'urn:cite:perseus:upload.K6BGkIMwMoR';
-var max_width = 200;
-var max_height = 200;
-
-# Get the upload src
-
-var upload_src = null;
-json.urn( upload_urn ).then( 
-function( data ){
-	upload_src = data['src'][0];
-});
-
-# Get the upload json file
-
-var upload_json = null;
-json.get( upload_src ).then(
-function( data ){
-	upload_json = data;
-});
-
-# Get a fresh resize_urn
-
-var resize_urn = null;
-urnServ.fresh( urnServ.base+"resize.{{ id }}", function( urn ){
-	resize_urn = urn;
-});
-
-# Get the resize json template
-
-var tmpl = null;
-json.get( 'default/resize.json' ).then( function( data ){
-	tmpl = data;
-});
-
-# Fill in the blanks
-
-tmpl['@id'] = resize_urn;
-tmpl[onto.with_prefix('memberOf')]['@id'] = upload_json['@id'];
-
-# Send it off
-
-var conf = tserv('config');
-var save_to = conf.xhr.json.url+'/data/resize/'+resize_urn;
-var img = upload_json[ onto.with_prefix('src') ]['@id'];
-imgup.resize( img, 200, 200, save_to, tmpl ).then( function( data ){
-	console.log( data );
-});
-
-
+var r = tserv('resizer')
+r.add('urn:cite:perseus:resize.ry897TREW', 200, 200)
+r.add('urn:cite:perseus:upload.QAlWThSWNU1', 200, 200)
 
 */
