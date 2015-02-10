b2ea67b21cf894ccfe7455f64117cb6f7ef2cf01
caesarfeta <adamtavares@gmail.com>
2015-02-10 16:05:54 -0500

    All list views now use the new query service.  ListCtrl is dead!!

--- angular/lib/ctrl/collection.js
+++ angular/lib/ctrl/collection.js
@@ -63,48 +63,111 @@ function( $scope, $injector, urnServ, $rootScope, user, onto ){
 // collections
 
 appControllers.controller( 'CollectionListCtrl', [
-  '$scope',
-  '$injector',
-  'user',
-  '$rootScope',
-  'onto', 
-  function( $scope, $injector, user, $rootScope, onto ){
-    
-    $scope.type = "collection";
-    $scope.title = "Collection List";
-    $scope.keys = [ 'urn','label','desc','user','time' ];
+'$scope',
+'$injector',
+'$rootScope',
+'$routeParams',
+'user',
+'onto',
+'query',
+function( $scope, $injector, $rootScope, $routeParams, user, onto, query ){
+  $scope.type = "collection";
+  $scope.keys = [ 'urn','label','desc','user','time' ];
+	$scope.limit = 10;
+  $scope.page = ( $routeParams.page == undefined ) ? 1 : parseInt( $routeParams.page );
 
-    
-    var label = onto.with_prefix('label');  
-    var desc = onto.with_prefix('description');
-    
-    // Run after user has been authorized
-    
-    $rootScope.$on( user.events.ok, function(){ go() });
-    function go(){
-      
-      // Inherit from parent
+  // The fields you allow users to filter
+  // are set with object keys in $scope.filter
 
-      $injector.invoke( ListCtrl, this, { $scope: $scope } );
-      $scope.init([label,desc]);
+  var label = onto.with_prefix('label');  
+  var desc = onto.with_prefix('description');
+  $scope.filter = {};
+  $scope.filter[label] = null;
+  $scope.filter[desc] = null;
+
+	$scope.query = function(){
+		return {
+			where: [
+				[ '?urn', 'type', '"collection"' ],
+				user_check(),
+				[ '?urn', 'label', '?label', build_filter( '?label', label ) ],
+				[ '?urn', 'description', '?desc', build_filter( '?desc', desc ) ],
+				[ '?urn', 'created', '?time', { optional:true } ],
+				[ '?urn', 'represents', '?rep', { optional:true } ],
+				[ '?urn', 'creator', '?user', { optional:true } ],
+			],
+			order_by: 'desc( ?time )',
+			limit: $scope.limit,
+			offset: $scope.limit * ( $scope.page-1 )
+		}
+	}
+
+	function user_check(){
+		if ( user.only ) {
+			return [ '?urn', 'creator', "<"+user.url()+">" ];
+		}
+		return null;
+	}
+
+	// Build a filter clause
+
+	function build_filter( key, handle ){
+		var filter = $scope.filter[ handle ]
+		if ( filter == null ){
+			return angular.copy( { optional:true } );
+		}
+		return angular.copy({ filter:'regex( '+key+', "'+ filter +'", "i" )' });
+	}
+
+  // Start once user event fires 
+
+  $rootScope.$on( user.events.ok, 
+    function(){ $scope.apply_filter() }
+	);
+
+  // Clear the filter
+
+  $scope.clear_filter = function(){
+    for ( var key in $scope.filter ){
+      $scope.filter[key] = null;
     }
-    
-    // The fields you allow users to filter
-    // are set with object keys in $scope.filter
-    
-    // See lib/list_ctr.js: filter()
-    $scope.filter = {};
-    $scope.filter[label] = null;
-    $scope.filter[desc] = null;
-    
-    // Applying the filter is the same as initializing..
-    
-    $scope.apply_filter = function(){
-      $injector.invoke( ListCtrl, this, { $scope: $scope } );
-      $scope.init([label,desc]);
-    }
+    $scope.apply_filter();
   }
-]);
+
+
+  // Filtering changes if user.only changes
+
+  $scope.$watch( function(){ return user.only },
+    function( newVal, oldVal ){
+      if ( newVal != oldVal ){
+        $scope.apply_filter();
+      }
+    }
+  );
+
+  // Applying the filter is the same as initializing..
+
+  $scope.apply_filter = function(){
+	
+		// Get count
+	
+		query.count( $scope.query() ).then(
+		function( data ){
+			$scope.count = data;
+      $scope.pages = Math.ceil( $scope.count / $scope.limit );
+      $scope.prev = ( $scope.page > 1 ) ? true : false;
+      $scope.next = ( $scope.page < $scope.pages ) ? true : false;
+		});
+	
+		// Get the data
+	
+    query.get( $scope.query() ).then( 
+		function( data ){
+			$scope.json = data;
+		});
+  }
+	
+}]);
 
 
 // collection/:urn
--- angular/lib/ctrl/item.js
+++ angular/lib/ctrl/item.js
@@ -1,38 +1,113 @@
 // items
 
 appControllers.controller( 'ItemListCtrl', [
-  '$scope',
-  '$injector',
-  'onto',
-  function( $scope, $injector, onto ){
-    $scope.type = "item";  
-    $scope.title = "Item List";
-    $scope.keys = [ 'urn','label','desc','user','time' ];
-    $injector.invoke( ListCtrl, this, { $scope: $scope } );
-    
-    // The fields you allow users to filter
-    // are set with object keys in $scope.filter
-    //
-    // See lib/list_ctr.js: filter()
-  
-    var label = onto.with_prefix('label');  
-    var desc = onto.with_prefix('description');
-    var rep = onto.with_prefix('represents');
-    $scope.init([label,desc,rep]);
-    $scope.filter = { };
-    $scope.filter[label] = null;
-    $scope.filter[desc] = null;
-    $scope.filter[rep] = null;
-    
-    
-    // Applying the filter is the same as initializing..
-    
-    $scope.apply_filter = function(){
-      $injector.invoke( ListCtrl, this, { $scope: $scope } );
-      $scope.init([label,desc,rep]);
+'$scope',
+'$injector',
+'$rootScope',
+'$routeParams',
+'user',
+'onto',
+'query',
+function( $scope, $injector, $rootScope, $routeParams, user, onto, query ){
+  $scope.type = "item";
+  $scope.keys = [ 'urn','label','desc','rep','user','time' ];
+	$scope.limit = 10;
+  $scope.page = ( $routeParams.page == undefined ) ? 1 : parseInt( $routeParams.page );
+
+  // The fields you allow users to filter
+  // are set with object keys in $scope.filter
+
+  var label = onto.with_prefix('label');  
+  var desc = onto.with_prefix('description');
+  var rep = onto.with_prefix('represents');
+  $scope.filter = {};
+  $scope.filter[label] = null;
+  $scope.filter[desc] = null;
+  $scope.filter[rep] = null;
+
+	$scope.query = function(){
+		return {
+			where: [
+				[ '?urn', 'type', '"item"' ],
+				user_check(),
+				[ '?urn', 'label', '?label', build_filter( '?label', label ) ],
+				[ '?urn', 'description', '?desc', build_filter( '?desc', desc ) ],
+				[ '?urn', 'represents', '?rep', build_filter( '?rep', rep ) ],
+				[ '?urn', 'created', '?time', { optional:true } ],
+				[ '?urn', 'creator', '?user', { optional:true } ],
+			],
+			order_by: 'desc( ?time )',
+			limit: $scope.limit,
+			offset: $scope.limit * ( $scope.page-1 )
+		}
+	}
+
+	function user_check(){
+		if ( user.only ) {
+			return [ '?urn', 'creator', "<"+user.url()+">" ];
+		}
+		return null;
+	}
+
+	// Build a filter clause
+
+	function build_filter( key, handle ){
+		var filter = $scope.filter[ handle ]
+		if ( filter == null ){
+			return angular.copy( { optional:true } );
+		}
+		return angular.copy({ filter:'regex( '+key+', "'+ filter +'", "i" )' });
+	}
+
+  // Start once user event fires 
+
+  $rootScope.$on( user.events.ok, 
+    function(){ $scope.apply_filter() }
+	);
+
+  // Clear the filter
+
+  $scope.clear_filter = function(){
+    for ( var key in $scope.filter ){
+      $scope.filter[key] = null;
     }
+    $scope.apply_filter();
   }
-]);
+
+
+  // Filtering changes if user.only changes
+
+  $scope.$watch( function(){ return user.only },
+    function( newVal, oldVal ){
+      if ( newVal != oldVal ){
+        $scope.apply_filter();
+      }
+    }
+  );
+
+  // Applying the filter is the same as initializing..
+
+  $scope.apply_filter = function(){
+
+		// Get count
+
+		query.count( $scope.query() ).then(
+		function( data ){
+			$scope.count = data;
+      $scope.pages = Math.ceil( $scope.count / $scope.limit );
+      $scope.prev = ( $scope.page > 1 ) ? true : false;
+      $scope.next = ( $scope.page < $scope.pages ) ? true : false;
+		});
+
+		// Get the data
+
+    query.get( $scope.query() ).then( 
+		function( data ){
+			$scope.json = data;
+		});
+  }
+
+}]);
 
 
 // item/:urn
