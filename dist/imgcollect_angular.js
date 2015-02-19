025b67a06c93434305a482083ede19ffd8420692
caesarfeta <adamtavares@gmail.com>
2015-02-19 16:31:44 -0500

    New upload with progress bar added.  Controller is still messy though.  Will refactor a bit before replacing existing new upload page.

--- dist/imgcollect_angular.js
+++ dist/imgcollect_angular.js
@@ -3087,9 +3087,15 @@ appControllers.controller( 'UploadNew2', [
 'tmpl',
 'resizer',
 '$upload',
-function( $scope, config, json, tmpl, resizer, $upload ){
+'onto',
+'urnServ',
+'user',
+'resizer',
+function( $scope, config, json, tmpl, resizer, $upload, onto, urnServ, user, resizer ){
 	
-	$scope.title = "Upload New 2";
+	$scope.type = 'upload';
+	
+	// UPLOAD
 	
 	$scope.progress = {
 		total: null,
@@ -3097,6 +3103,7 @@ function( $scope, config, json, tmpl, resizer, $upload ){
 		done: false,
 		error: false
 	};
+	
 	$scope.default = angular.copy( $scope.progress );
 	
 	var pbar = $( '#progressbar' );
@@ -3128,8 +3135,104 @@ function( $scope, config, json, tmpl, resizer, $upload ){
 			$scope.progress.orig = r.orig;
 			$scope.progress.src = r.src;
 			$scope.progress.done = true;
+	  	urnServ.fresh( urnServ.base+"upload.{{ id }}", fresh_callback );
 		})
 	}
+	
+  var fresh_callback = function( urn ){
+    $scope.urn = urn;
+		load_tmpl();
+	}
+	
+	// METATADATA
+	
+	$scope.tmpl = {
+		loaded: null,
+		error: null,
+		saving: false,
+		saved: false
+	};
+	
+	function load_tmpl(){
+		tmpl.get( $scope.type ).then(
+		function( r ){
+			$scope.json = r;
+			$scope.tmpl.loaded = true;
+		}),
+		function( err ){
+			$scope.tmpl.error = err;
+		};
+	}
+	
+  var label = onto.with_prefix('label');  
+  var desc = onto.with_prefix('description');
+  var src = onto.with_prefix('src');
+  var rights = onto.with_prefix('rights');
+  var owner = onto.with_prefix('owner');
+  
+  $scope.form = {};
+  $scope.form[ label ] = "";
+  $scope.form[ desc ] = "";
+  $scope.form[ owner ] = "";
+  $scope.form[ rights ] = "";
+  $scope.form[ src ] = "";
+  $scope.src_field = src;
+	
+	$scope.edit_fields = [ label, desc, rights, owner ];
+	
+  // Update the form with JSON data
+  
+  function form() {
+  	for ( var key in $scope.json ) {
+      if ( key in $scope.form ) {
+        $scope.form[key] = $scope.json[key];
+      }
+  	}
+  }
+	
+  // Update JSON when form changes
+  
+  $scope.change = function( key ) {
+    if ( key in $scope.json ) {
+      if ( angular.isDefined( $scope.json[key]['@id'] ) ){
+        $scope.json[key]['@id'] = $scope.form[key];
+      } 
+			else {
+        $scope.json[key] = $scope.form[key];
+      }
+    }
+  }
+	
+  // Build the data path URL
+  
+  $scope.data_path = function( urn ){
+    return $scope.type+'/'+urn
+  }
+  
+  
+  // Save the default after writing the most basic values
+  
+	$scope.save = function(){
+		$scope.tmpl.saving = true;
+    touch();
+    json.post( $scope.data_path( $scope.urn ), $scope.json ).then(
+    function( data ){
+			$scope.tmpl.saved = true;
+			resizer.add( $scope.urn, 200, 200 );
+    });
+  }
+  
+  
+  // Set basic values
+  
+	function touch(){
+    $scope.json['@id'] = $scope.urn;
+    var creator = onto.with_prefix('creator');
+    var created = onto.with_prefix('created');
+    $scope.json[creator]['@id'] = user.id();
+    $scope.json[created] = ( new TimeStamp ).xsd();
+		$scope.json[src]['@id'] = $scope.progress.src;
+  }
 	
 	
 }]);
